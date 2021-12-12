1a5ad20f6288e5e35109b275a221730c0f9e1110
Francisco Carranza <francar94@gmail.com>
2021-12-13 00:23:10 +0100

    User model removed old id reference

--- src/infrastructure/orm/sequelize/models/User.ts
+++ src/infrastructure/orm/sequelize/models/User.ts
@@ -2,20 +2,13 @@ import { DataTypes, Model, Optional } from 'sequelize';
 import { sequelize } from '.';
 import { User } from '../../../../modules/users/domain/User';
 
-interface UserCreationAttributes extends Optional<User, 'id'> {}
+interface UserCreationAttributes extends Optional<User, 'uuid'> {}
 
 export interface UserInstance extends Model<User, UserCreationAttributes> {}
 
 const UserModel = sequelize.define<UserInstance>(
   'User',
   {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER.UNSIGNED,
-      unique: true,
-    },
     uuid: {
       allowNull: false,
       primaryKey: true,
